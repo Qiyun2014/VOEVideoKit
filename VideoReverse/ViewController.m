24f8d5f0412c13a4936a20aa5ef499da7be762a1
祁云 <shagengba@gmail.com>
2020-04-17 18:11:07 +0800

    update

--- VideoReverse/ViewController.m
+++ VideoReverse/ViewController.m
@@ -20,18 +20,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    // 读取视频
     NSString *file = [[NSBundle mainBundle] pathForResource:@"IMG_0007" ofType:@"mp4"];
     VOEVideo *videoHandle = [VOEVideo videoForURL:[NSURL fileURLWithPath:file]];
     VOEVideoReader *videoReader = [[VOEVideoReader alloc] initWithVideo:videoHandle];
     
-    
+    // 创建新视频
     NSString *outputFile = [VOEVideoWriter createTempFileWithFormat:@"mp4"];
     NSLog(@"file = %@", outputFile);
     VOEVideoWriter *writerHandle = [[VOEVideoWriter alloc] initWithURL:[NSURL fileURLWithPath:outputFile]];
     writerHandle.naturesize = videoReader.getVideoHandle.naturalSize;
     writerHandle.videoReader = videoReader;
     
+    // 解码每一帧视频
     [videoReader startDecompressionVideoWithPrepareBlock:^{
+        // 编码每一帧视频到指定格式的文件，默认是quick time movie
         [writerHandle startWriter];
     }];
 
